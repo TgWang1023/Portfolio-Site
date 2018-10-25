f38b06bc20e628d533b4a9b87e8215810aa3e8d0
Tiange Wang <wtgkevin@gmail.com>
2018-10-25 13:10:07 -0700

    remake images, added new routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,4 +2,25 @@ module.exports = function(app){
     app.get('/', function(request, response){
         response.render('index');
     })
+    app.get('/express_scratch', function(request, response){
+
+    });
+    app.get('/hello_world', function(request, response){
+
+    });
+    app.get('/neighborhood', function(request, response){
+
+    });
+    app.get('/my_travel', function(request, response){
+
+    });
+    app.get('/simple_duel', function(request, response){
+
+    });
+    app.get('/crazy_maze', function(request, response){
+
+    });
+    app.get('*', function(request, response){
+        response.redirect('/');
+    });
 };
