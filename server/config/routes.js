5a48ddfd99de6026fff4f903ee78e4456d573958
Tiange Wang <wtgkevin@gmail.com>
2018-10-25 13:31:26 -0700

    minor changes

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,22 +3,22 @@ module.exports = function(app){
         response.render('index');
     })
     app.get('/express_scratch', function(request, response){
-
+        response.render('expsrc');
     });
     app.get('/hello_world', function(request, response){
-
+        response.render('helwo');
     });
     app.get('/neighborhood', function(request, response){
-
+        response.render('neigh');
     });
     app.get('/my_travel', function(request, response){
-
+        response.render('mytrv');
     });
     app.get('/simple_duel', function(request, response){
-
+        response.render('simdu');
     });
     app.get('/crazy_maze', function(request, response){
-
+        response.render('/crzma');
     });
     app.get('*', function(request, response){
         response.redirect('/');
