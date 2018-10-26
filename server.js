a66bbfe0482170b501de15accc263fb5a80431a2
Tiange Wang <wtgkevin@gmail.com>
2018-10-26 16:06:52 -0700

    minor changes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const favicon = require('serve-favicon');
 const app = express();
 
+app.use(favicon(__dirname + '/favicon.ico'));
 app.use(express.static(__dirname + "/client/static"));
 app.set('views', __dirname + '/client/views');
 app.set('view engine', 'ejs');
